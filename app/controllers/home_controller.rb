6c575600c35686bcc97ef678bd0091a92cd397e9
Marina Kir <marina@MacBook-de-Marina.local>
2017-08-10 16:59:38 +0200

    add flash

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+	def index
+  	    tweet = params[:tweet_text]
+		unless tweet == nil
+    	    SendTweet.new(tweet).perform
+    	   	flash[:success] = "Tweet sent out to the world :)"
+    	    redirect_to root_path
+    	end
+	end
+end
