4c86e42f15131635f0fcc1c0dde99ba12e528377
Marina Kir <marina@MacBook-de-Marina.local>
2017-08-10 15:11:16 +0200

    tweet service working + new controller home

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -1,12 +1,12 @@
-class SendTweet
-	require 'twitter'
-	require 'dotenv/load'
+require 'dotenv/load'
 
-	def initialize
+class SendTweet
+	def initialize(tweet)
+		@tweet = tweet.to_s
 	end
 
 	def log_in_to_twitter
-		client = Twitter::REST::Client.new do |config|
+		@client = Twitter::REST::Client.new do |config|
 		  config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
 		  config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
 		  config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
@@ -14,12 +14,12 @@ class SendTweet
 		end
 	end
 
-	def send_tweet(tweet)
-		client.update('tweet')
+	def send_tweet
+		@client.update(@tweet)
 	end
 
-	def perform(tweet)
+	def perform
 		log_in_to_twitter
-		send_tweet(tweet)
+		send_tweet
 	end
 end
