222ca80840562b0345a3076c7f899bd89b5e21c5
Marina Kir <marina@MacBook-de-Marina.local>
2017-08-10 14:53:00 +0200

    send_tweet.rb v1

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -1,28 +1,25 @@
 class SendTweet
 	require 'twitter'
 	require 'dotenv/load'
-	
+
 	def initialize
 	end
 
 	def log_in_to_twitter
-		
+		client = Twitter::REST::Client.new do |config|
+		  config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
+		  config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
+		  config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
+		  config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
+		end
 	end
 
-	def send_tweet
-		
+	def send_tweet(tweet)
+		client.update('tweet')
+	end
+
+	def perform(tweet)
+		log_in_to_twitter
+		send_tweet(tweet)
 	end
 end
-
-
-
-require 'twitter'
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
-  config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
-end
-
-client.update('Bonjour monde ! comment va ?')
